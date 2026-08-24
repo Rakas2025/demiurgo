e36d726242009db7a624b0d5da79fa82aa00edc3
Rakas2025 <195011520+Rakas2025@users.noreply.github.com>
2026-08-25 01:53:17 +0200

    Update character sheet class for new system

--- module/sheets/character-sheet.mjs
+++ module/sheets/character-sheet.mjs
@@ -1,8 +1,8 @@
 export class CharacterSheet extends ActorSheet {
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["mysystem", "sheet", "actor"],
-      template: "systems/mysystem/templates/actor/character-sheet.hbs",
+      classes: ["demiurgo", "sheet", "actor"],
+      template: "systems/demiurgo/templates/actor/character-sheet.hbs",
       width: 700,
       height: 600,
       tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "attributes" }]
