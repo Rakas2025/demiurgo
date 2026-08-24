53d4a41aa586c99e0cf7ca70dc709d313785f017
Rakas2025 <195011520+Rakas2025@users.noreply.github.com>
2026-08-25 01:11:02 +0200

    Update mysystem.mjs

--- mysystem.mjs
+++ mysystem.mjs
@@ -1,1 +1,15 @@
+import { CharacterData } from "./module/data-models.mjs";
+import { CharacterSheet } from "./module/sheets/character-sheet.mjs";
 
+Hooks.once("init", async function() {
+  console.log("MySystem | Initializing System");
+
+  CONFIG.Actor.dataModels.character = CharacterData;
+
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("mysystem", CharacterSheet, {
+    types: ["character"],
+    makeDefault: true,
+    label: "Ficha de personaje"
+  });
+});
