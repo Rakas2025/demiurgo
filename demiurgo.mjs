5d1b25bc22c7714b8fdb1f1a914ed634f3897e86
Rakas2025 <195011520+Rakas2025@users.noreply.github.com>
2026-08-25 02:00:17 +0200

    Update demiurgo.mjs

--- demiurgo.mjs
+++ demiurgo.mjs
@@ -1,8 +1,11 @@
 import { CharacterData } from "./module/data-models.mjs";
+import { MySystemActor } from "./module/documents.mjs";
 import { CharacterSheet } from "./module/sheets/character-sheet.mjs";
 
 Hooks.once("init", async function() {
   console.log("Demiurgo | Initializing System");
+
+  CONFIG.Actor.documentClass = MySystemActor;
 
   CONFIG.Actor.dataModels.character = CharacterData;
 
